c1d584d238b84b263997afab66535be142064966
manisha-ghb <ppidugumanisha@gmail.com>
2025-01-29 22:21:36 +0530

    python

---
+++ Task1.py
@@ -0,0 +1,58 @@
+print("ARITHMETIC OPERATIONS")
+print(8+5)
+print(8-5)
+print(8*5)
+print(8/5)
+print(8//5)
+#Exponent
+print(5**8)
+#variables
+a = 85
+b = 58
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+#Changing "b" value
+b = 35
+c = 94.93
+print(type(b))
+print(type(c))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+pooji = 94
+print(pooji)
+print("Manisha")
+str = "Mani_ @123"
+print(len(str))
+#indexing
+print(str[0])
+print(str[5])
+print(str[-1])
+print(str[-4])
+#slicing operation
+print(str[0:5])
+print(str[6:11])
+#step index
+print(str[0::2])
+#slicing with strings (both +ve and -ve indexing)
+str = "Ali khan, Tappu na vaipu vundhi kabatti....kh"
+print(len(str))
+print(str[ : ])
+print(str[ :-38:3])
+print(str[0:42])
+print(str[-1:-8:-1])
+print(str[-45:-1])
+print(str[6:45:6])
+#To print memory address of number or string
+#Here "id" refers to memory block--> it will print the address of number 5.
+num1=5
+print(id(num1)) 
+str1="Mani"
+print(id(str1))
+str2="Mani"
+print(id(str2))
